83fd83551d294d78c149cb1ff37344cb84027704
Eugine <eugine_m@outlook.in>
2026-07-24 14:37:00 +0530

    Create Radar Chart Dataset.js

---
+++ Radar Chart Dataset.js
@@ -0,0 +1,20 @@
+{
+  "labels": [
+    "Product",
+    "Business",
+    "Competitors",
+    "Research",
+    "Segments",
+    "Problems",
+    "Prioritization",
+    "Solutions",
+    "Metrics",
+    "PRD"
+  ],
+  "datasets": [
+    {
+      "label": "PM Maturity",
+      "data": [4,3,4,5,4,5,4,3,2,4]
+    }
+  ]
+}
